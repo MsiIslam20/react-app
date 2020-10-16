4d6016297477cf41adc32ce95cb8e8a6a7cd145b
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-16 22:30:57 +0600

    payment option added

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {Elements} from '@stripe/react-stripe-js';
+import {Elements, CardElement} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
-import {CardElement} from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
 
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -4,43 +4,52 @@ import './Shipment.css'
 import { useContext } from 'react';
 import { userContext } from '../../App';
 import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import ProcessPayment from '../ProcessPayment/ProcessPayment';
 
 const Shipment = () => {
 
-    const [loggedInUser, setLoggedInUser] = useContext(userContext);
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = data => {
-      const savedCart = getDatabaseCart();
-      const orderDetail = {...loggedInUser, products : savedCart, shipment : data, orderTime : new Date()};
-      fetch("https://calm-garden-46705.herokuapp.com/addOrder", { 
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(orderDetail)
-      })
+  const [loggedInUser, setLoggedInUser] = useContext(userContext);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+    const savedCart = getDatabaseCart();
+    const orderDetail = { ...loggedInUser, products: savedCart, shipment: data, orderTime: new Date() };
+    fetch("https://calm-garden-46705.herokuapp.com/addOrder", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(orderDetail)
+    })
       .then(res => res.json())
-      .then(data =>{
-        if(data){
+      .then(data => {
+        if (data) {
           processOrder();
           alert("Your Order Place Successfully")
         }
       })
-    };
-  
-    return (
-      <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-        <input name="Name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
-        {errors.Name && <span className="error">Name is required!</span>}
-        <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
-        {errors.email && <span className="error">Email is required@</span>}
-        <input name="address" ref={register({ required: true })} placeholder="Address" />
-        {errors.address && <span className="error">Address is required!!</span>}
-        <input name="phone" ref={register({ required: true })} placeholder="Phone Number" />
-        {errors.phone && <span className="error">Phone is required!!</span>}
-        <input type="submit" />
-      </form>
-    );
+  };
+
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+          <input name="Name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
+          {errors.Name && <span className="error">Name is required!</span>}
+          <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
+          {errors.email && <span className="error">Email is required@</span>}
+          <input name="address" ref={register({ required: true })} placeholder="Address" />
+          {errors.address && <span className="error">Address is required!!</span>}
+          <input name="phone" ref={register({ required: true })} placeholder="Phone Number" />
+          {errors.phone && <span className="error">Phone is required!!</span>}
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="col-md-6 pr-4">
+        <h2 className="mb-5">Please Proceed your payment..</h2>
+        <ProcessPayment />
+      </div>
+    </div>
+  );
 };
 
 export default Shipment;
