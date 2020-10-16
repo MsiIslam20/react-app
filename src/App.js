e4cac543701ba6f0dde0ef2c5f91ec88fe1fb4e2
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-16 17:08:14 +0600

    payment gateway added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Order from './components/Order/Order';
 import Inventory from './components/Inventory/Inventory';
